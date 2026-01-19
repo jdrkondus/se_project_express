2754d9d2abeec4849f40ce5350b924eeb02f8e2b
Jules Archondous <jdrkondus@gmail.com>
2026-01-18 20:20:12 -0600

    syntax fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,12 +3,11 @@ const token = authorization.replace("Bearer ", "");
 payload = jwt.verify(token, JWT_SECRET);
 req.user = { _id: payload._id };
 next();
+try {
+  jwt.verify(token, JWT_SECRET);
 } catch (err) {
-res.status(401).send({ message: "Authorization required" });
+  res.status(401).send({ message: "Authorization required" });
 }
-} else {
-res.status(401).send({ message: "Authorization required" });
-}
-};
+;
 
 module.exports = auth;
